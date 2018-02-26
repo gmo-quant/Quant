a4f71e265f1a47db17449d76d91e8e92a6b79dc5
GuangzhuanMo <quan.fin.mo@gmail.com>
2018-02-26 15:53:24 -0500

    save before a big change

--- playGround/oneOptLCPLG.py
+++ playGround/oneOptLCPLG.py
@@ -1,47 +1,46 @@
 
-#
-# draw  a Profit/ loss diragram for a one option strategy.
-#
+# profit/loss graph for one option long call strategy
+
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-
-def plotCallPLG(stockPrice, plSeries):
-		
-	plt.figure(figsize=(16, 10))
-	plt.title('PLG for call')
-	plt.plot(stockPrice, plSeries)
-	plt.ylabel("Profit/Loss")
-	plt.xlabel("stock price @ expiration")
-	plt.axhline(y=0, color='g', linestyle='-')
-	plt.show()
-def generateCallPLSeries(stockPrice, strikePrice, initPremium):
-
-	# need to implement a pandas.Series version 
-	# later on
-	# currently, implement in simple list manner
-	#plSeries = pd.Series(stockPrice['2002-02':'2002-12'])
-	#plSeries=stockPrice['2002-02':'2002-12']
-	#for price in plSeries: 
-	plSeries = []
-	for price in stockPrice:
-		if isInTheMoney(price, strike):
-			plSeries.append(price)
-		else:
-			plSeries.append(0)
+def generatePLSeries(strike, currentPrice, initPremium):
+	instrinsicVal = currentPrice -  strike;
+	zeroInstrinsic = instrinsicVal  < 0;
+	instrinsicVal[zeroInstrinsic] = 0;
+	plSeries = instrinsicVal - initPremium;
+	
 	return plSeries
 
-def isInTheMoney(price, strike):
-	if price > strike:
-		return True
-	else:
-		return False
-
+def plotOneLCPLG(plSeries):
+	
+	plt.figure(figsize=(16, 10))
+	plt.title('PLG for one option long call strategy')
+	plt.ylabel("Profit/Loss")
+	plt.xlabel("stock price @ expiration")
+	plSeries.xhline(y=0, color='g', linestyle='-')
+	plSeries.plot()
+	plt.show()
 
 msft = pd.read_csv("../data/msft_2000_2017.csv", index_col=0, parse_dates=True)
+price = msft.Close['2002-02':'2002-03']
+plSeries = price.copy()
+initPremium = 2
+strike = 31
+instrinsicVal = plSeries - strike
+zeroInstrinsic = instrinsicVal < 0
+instrinsicVal[zeroInstrinsic] = -initPremium 
 
-plSeries = generateCallPLSeries(msft.Close['2002-02':'2002-12'],1, 2 )
 
-plotCallPLG(plSeries)
+
+#price = [110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97, 96, 95]
+'''
+priceSerise = pd.Series(price)
+priceSerise -= 100
+priceSerise[priceSerise < 0] = -5 
+#plotOneLCPLG(instrinsicVal)
+plotOneLCPLG(priceSerise)
+'''
+
+plotOneLCPLG(instrinsicVal)
